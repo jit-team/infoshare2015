89af3fb4a2c065395439852f04fdee49f0d21ab6
Łukasz Młynik <lukasz.mlynik@hotmail.com>
2015-05-29 02:02:07 +0200

    Create a simple post form

---
+++ src/Infoshare.Workshop.Webapp/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Mvc;
+using Infoshare.Workshop.Webapp.Models;
+
+namespace Infoshare.Workshop.Webapp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginForm loginForm)
+        {
+            return View(loginForm);
+        }
+    }
+}
---
+++ src/Infoshare.Workshop.Webapp/Models/LoginForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infoshare.Workshop.Webapp.Models
+{
+    public class LoginForm
+    {
+        public string Login { get; set; }
+    }
+}
